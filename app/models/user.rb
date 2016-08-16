3c5ef73c121686088bfba6681d34aba0147edcb3
Ricardo Bernardeli <ricardobcs@gmail.com>
2016-08-17 09:09:48 +1000

    Getting a feel of how many times dump/load are called

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+class OjSerializer
+  def self.dump(hash)
+    puts "dump"
+    Oj.dump(hash)
+  end
+
+  def self.load(json)
+    puts "load"
+    Oj.load(json)
+  end
+end
+
 class User < ActiveRecord::Base
-  serialize :data, Oj
+  serialize :data, OjSerializer
 end
